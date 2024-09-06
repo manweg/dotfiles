21d94861211965fd8c6259b2e296e2b2851696a9
mwegel <manuel.wegel@sva.de>
2024-09-06 10:14:27 +0200

    add xml language server

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,7 +7,14 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "xml",
       -- add more arguments for adding more treesitter parsers
     },
+    highlight = {
+      enable = true, -- Highlighting aktivieren
+    },
+    indent = {
+      enable = true, -- Indentierung aktivieren
+    }
   },
 }
