f928a8e0af67a9408f78bdcb93a2de787ba028af
mwegel <manuel.wegel@sva.de>
2024-09-10 09:02:54 +0200

    add spacebar / test new sketchybar

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,18 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
-source "$CONFIG_DIR/colors.sh" # Loads all defined colors
-
-if [ $SELECTED = true ]; then
-  sketchybar --set $NAME background.drawing=on \
-                         background.color=$ACCENT_COLOR \
-                         label.color=$BAR_COLOR \
-                         icon.color=$BAR_COLOR
+if [ "$SELECTED" = "true" ]; then
+  sketchybar -m --set $NAME background.color=0xff81a1c1
 else
-  sketchybar --set $NAME background.drawing=off \
-                         label.color=$ACCENT_COLOR \
-                         icon.color=$ACCENT_COLOR
+  sketchybar -m --set $NAME background.color=0xff57627A
 fi
