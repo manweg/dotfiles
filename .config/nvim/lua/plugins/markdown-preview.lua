4bdf9d5606066f54dbc3a9bcee372ace84ac2ddb
mwegel <manuel.wegel@sva.de>
2024-09-20 12:39:45 +0200

    change markdown preview plugin to not use yarn

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,9 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function() vim.g.mkdp_filetypes = { "markdown" } end,
   ft = { "markdown" },
+  build = function()
+    require("lazy").load { plugins = { "markdown-preview.nvim" } }
+    vim.fn["mkdp#util#install"]()
+  end,
 }
