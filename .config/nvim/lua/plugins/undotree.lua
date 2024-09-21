b0725b849d6299998839689b6654e81fd79b0962
mwegel <manuel.wegel@sva.de>
2024-09-22 00:25:36 +0200

    fix zen mode

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -1,8 +1,7 @@
 return {
   "mbbill/undotree",
-    config = function()
-    vim.keymap.set('n', '<leader>z', vim.cmd.UndotreeToggle, { desc = "Open undotree"})
+  config = function()
+    vim.keymap.set("n", "<leader>y", vim.cmd.UndotreeToggle, { desc = "Open undotree" })
     -- vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add file" })
-  end
+  end,
 }
-
--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -1,9 +1,21 @@
 -- Lua
+-- return {
+--   "folke/zen-mode.nvim",
+--   opts = {
+--     -- your configuration comes here
+--     -- or leave it empty to use the default settings
+--     -- refer to the configuration section below
+--   },
+-- }
 return {
   "folke/zen-mode.nvim",
+  cmd = "ZenMode",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      gitsigns = true,
+      tmux = true,
+ --     wezterm = { enabled = false, font = "+2" },
+    },
   },
+  keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
 }
