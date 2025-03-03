0b3fa8297ea36870ccbc65c1bd6b045fe2dd1495
Manuel Wegel <manuel.wegel@bambf.bund.de>
2025-03-03 14:39:58 +0100

    change sessionzier dirs and colortheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -868,16 +868,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'folke/tokyonight.nvim',
-    'arcticicestudio/nord-vim',
-    transparent = true,
+    'folke/tokyonight.nvim',
+    -- 'arcticicestudio/nord-vim',
+    transparent = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'nord'
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'nord'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -1007,10 +1007,10 @@ require 'user.mappings'
 -- Open Netrw in Tree Mode
 -- vim.g.netrw_liststyle = 3
 
-vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-vim.cmd [[hi NormalNC guibg=NONE ctermbg=NONE]] -- Optional für Inaktiv-Fenster
-vim.cmd [[hi LineNr guibg=NONE ctermbg=NONE]] -- Zeilennummern transparent
-vim.cmd [[hi SignColumn guibg=NONE]] -- Git/LSP-Symbole transparent
+-- vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
+-- vim.cmd [[hi NormalNC guibg=NONE ctermbg=NONE]] -- Optional für Inaktiv-Fenster
+-- vim.cmd [[hi LineNr guibg=NONE ctermbg=NONE]] -- Zeilennummern transparent
+-- vim.cmd [[hi SignColumn guibg=NONE]] -- Git/LSP-Symbole transparent
 
 -- Basic Settings
 vim.opt_local.cursorcolumn = true -- Highlight the current column
