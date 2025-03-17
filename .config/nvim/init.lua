62abe037a844dabd50dbd18f7cdd4d122e3ee93d
Manuel Wegel <manuel.wegel@bambf.bund.de>
2025-03-17 08:50:07 +0100

    disable yamlls language-server

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1038,48 +1038,26 @@ local function is_helm_chart(filepath)
   return chart_path:exists()
 end
 
-lspconfig.yamlls.setup {
-  on_attach = function(client, bufnr)
-    local filepath = vim.api.nvim_buf_get_name(bufnr)
-    if filepath:match '/templates/' and is_helm_chart(filepath) then
-      -- Deaktiviere yamlls für Helm Templates
-      client.stop()
-      return
-    end
-  end,
-  settings = {
-    yaml = {
-      -- schemaStore = {
-      -- enable = true,
-      --   url = 'https://www.schemastore.org/api/json/catalog.json',
-      -- },
-      validate = true,
-    },
-  },
-}
-
 -- LSP Configuration
 -- require('lspconfig').yamlls.setup {
+-- lspconfig.yamlls.setup {
+--   on_attach = function(client, bufnr)
+--     local filepath = vim.api.nvim_buf_get_name(bufnr)
+--     if filepath:match '/templates/' and is_helm_chart(filepath) then
+--       -- Deaktiviere yamlls für Helm Templates
+--       client.stop()
+--       return
+--     end
+--   end,
 --   settings = {
 --     yaml = {
---       format = {
---         enable = true, -- Automatische Formatierung aktivieren
+--       --       format = {
+--       --         enable = true, -- Automatische Formatierung aktivieren
+--       schemaStore = {
+--         enable = true,
+--         url = 'https://www.schemastore.org/api/json/catalog.json',
 --       },
---       schemas = {
---         kubernetes = 'k8s-*.yaml',
---         ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
---         ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
---         ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
---         ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
---         ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
---         ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
---         -- ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
---         ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
---         ['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
---         ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
---         ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
---         ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
---       },
+--       validate = true,
 --     },
 --   },
 -- }
