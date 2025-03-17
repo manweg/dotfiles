f1a58ce79e2a3cd5f16ba4a7bfcc1bc991360189
mwegel <manuel.wegel@sva.de>
2025-03-17 08:55:41 +0100

    add docs for nvim distros

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -935,7 +935,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml', 'helm' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'helm' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
