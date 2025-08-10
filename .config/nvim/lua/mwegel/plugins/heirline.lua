404fae06d3739e6263305d71ac302f3cb967357a
manweg <manuel.wegel@sva.de>
2025-08-10 20:25:14 +0200

    add heirline again

--- .config/nvim/lua/mwegel/plugins/heirline.lua
+++ .config/nvim/lua/mwegel/plugins/heirline.lua
@@ -2,9 +2,15 @@ return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
     opts.winbar = {
+      condition = function()
+        return vim.bo.filetype ~= "oil"
+      end,
+
+      -- 📄 Get just the filename
       init = function(self)
         self.filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
       end,
+
       {
         provider = function(self)
           return self.filename ~= "" and self.filename or "[No Name]"
@@ -13,11 +19,10 @@ return {
       },
     }
 
-    -- optional: disable statusline, tabline if you don’t need them
+    -- optional: disable statusline and tabline
     opts.statusline = nil
     opts.tabline = nil
 
     return opts
   end,
 }
-
